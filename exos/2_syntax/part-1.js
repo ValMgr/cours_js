91e88ebba430f39d2546e17d51a97f0e443d01bf
Valentin Magry <valou32720@gmail.com>
2021-10-07 09:09:46 +0200

    exo done

--- exos/2_syntax/part-1.js
+++ exos/2_syntax/part-1.js
@@ -7,9 +7,11 @@ console.log('');
 console.log("--- Bienvenue sur l'autoroute A7 ---");
 console.log('--- 1 ---');
 // créer une variable x, et lui assigner un nombre
+let x = 69;
 
 console.log('--- 2 ---');
 // logguer cette variable dans la console avec console.log()
+console.log(x)
 
 console.log('--- 3 ---');
 /*
@@ -17,18 +19,25 @@ console.log('--- 3 ---');
   en utilisant une boucle for.
 */
 const speeds = [23, 12, 57, 178, 129, 434, 222, 1000, 59, 887, 134];
+for (let i = 0; i < speeds.length; i++) {
+  console.log(speeds[i])
+}
 
 console.log('--- 4 ---');
 /*
   créer une fonction 'moins5' qui en entrée prend un nombre et renvoie le nombre moins 5
   puis logguez le résultat de la fonction 'moins5' appliquée sur x.
 */
-
+function moins5(input){
+  return input - 5;
+}
+console.log(moins5(x))
 console.log('--- 5 ---');
 /*
   changer la boucle de la question 3 pour corriger chaque nombre avec la fonction 'moins5'.
   Si le résultat est supérieur à 130, logguer 'Vous allez trop vite !'
 */
+speeds.forEach(s => { if(moins5(s) > 130) console.log('Vous allez trop vite :', moins5(s)); });
 
 console.log('');
 console.log('--- Merci de réinitialiser votre mot de passe ---');
@@ -39,21 +48,33 @@ console.log('--- 1 ---');
   Logguer le résultat de la fonction random appliquée sur x.
   ( Utiliser Math.random() et Math.floor() )
 */
-
+function getNumber(input){
+  return Math.floor(Math.random() * input);
+}
+console.log(getNumber(x));
 console.log('--- 2 ---');
 /*
   créer une fonction 'getLetter' qui renvoie une lettre au hasard parmi le tableau 'letters'.
 */
-
 const letters = ['abcdefghijklmnopqrstuvwxyz'];
-
+function getLetter(array){
+  return array[getNumber(array.length)];
+}
+console.log(getLetter(letters[0]));
 console.log('--- 3 ---');
 /*
   créer une fonction 'makePassword' qui en entrée prend un nombre 'size' et renvoie un mot de cette longueur
   construit avec des lettres au hasard.
   Utiliser la fonction 'getNumber', 'getLetter' ainsi qu'une boucle if.
 */
-
+function makePassword(size){
+  let r = "";
+  for (let i = 0; i < size; i++) {
+    r += getLetter(letters[0]);
+  }
+  return r;
+}
+console.log(makePassword(10))
 console.log('--- bonus ---');
 /*
   créer une fonction 'makeStrongerPassword' qui en entrée prend un nombre 'size' et un booléen 'withNumbers'
@@ -61,7 +82,19 @@ console.log('--- bonus ---');
   - Si 'withNumber' est vrai, faire en sorte qu'un caractère sur deux soit un nombre au hasard.
   - Si 'size' est plus petit que 8, logguer un message d'avertissement, mais créer le password quand même.
 */
-
+function makeStrongerPassword(size, withNumber){
+  let r = "";
+  for (let i = 0; i < size; i++) {
+    if(withNumber && i%2 === 0){
+      r += getNumber(10);
+    }else{
+      r += getLetter(letters[0]);
+    }
+  }
+  if(size < 8) console.log('Warning : password too short')
+  return r;
+}
+console.log(makeStrongerPassword(15, true));
 console.log('');
 console.log('--- Dis bonjour à mamie ---');
 console.log('--- 1 ---');
@@ -70,25 +103,60 @@ console.log('--- 1 ---');
   et qui renvoie une chaine de caractère de type Bonjour personnalisé (ex: Bonjour Romain).
   Utiliser cette fonction avec votre prénom en logguant son résultat
 */
-
+function bonjour(name){
+  return `Bonjour ${name}`;
+}
+console.log(bonjour('Valentin'));
 console.log('--- 2 ---');
 /*
   sur le même principe, créer une fonction 'bonsoir'
 */
-
+function bonsoir(name){
+  return `Bonsoir ${name}`;
+}
+console.log(bonsoir('Valentin'));
 console.log('--- 3 ---');
 /*
   créer une fonction 'saluer', qui en fonction de l'heure, utilise 'bonjour' ou 'bonsoir'.
   Utiliser new Date().getHours() pour avoir l'heure actuelle.
 */
-
+function saluer(){
+  let date = new Date().getHours()
+  return date < 18 ? bonjour('Valentin') : bonsoir('Valentin');
+}
+console.log(saluer())
 console.log('--- 4 ---');
 /*
 en utilisant le tableau de langues 'languages' ainsi qu'une boucle for,
 dire bonjour dans toutes les langues disponibles
 */
 const languages = ['fr', 'es', 'de', 'it', 'jp'];
-
+function polyglote(){
+  for (let i = 0; i < languages.length; i++) {
+    switch (languages[i]) {
+      case 'fr':
+        console.log('Bonjour');
+        break;
+      case 'es':
+        console.log('Holà');
+        break;
+      case 'de':
+        console.log('Hallo');
+        break;
+      case 'it':
+        console.log('Buongiorno')
+        break;
+      case 'jp':
+        console.log('Konnichiwa')
+        break;
+      default:
+        console.log('Hello')
+        break;
+    }
+    
+  }
+}
+polyglote();
 console.log('--- bonus ---');
 /*
 [] créer une fonction 'prepareHello' qui prend en entrée une langue 'lang' ('fr', 'it', 'es', 'de', ...)
@@ -97,3 +165,40 @@ console.log('--- bonus ---');
   Si on ne passe pas de langue, ou si la langue n'est pas reconnue, l'anglais doit être le défaut.
   Utiliser 'prepareHello' pour créer plusieurs fonctions pour dire bonjour dans plusieurs langues, et les essayer.
 */
+
+function prepareHello(lang){
+  switch (lang) {
+    case 'fr':
+      return (name) => {
+        return `Bonjour ${name}`
+      }
+    case 'es':
+      return (name) => {
+        return `Holà ${name}`
+      }
+    case 'de':
+      return (name) => {
+        return `Hallo ${name}`
+      }
+    case 'it':
+      return (name) => {
+        return `Buongiorno ${name}`
+      }
+    case 'jp':
+      return (name) => {
+        return `Konnichiwa ${name}`
+      }
+    default:
+      return (name) => {
+        return `Hello ${name}`
+      }
+      break;
+  }
+}
+
+let en = prepareHello('en');
+console.log(en('Valentin'));
+let es = prepareHello('es');
+console.log(es('Valentin'));
+let jp = prepareHello('jp');
+console.log(jp('Valentin'));
--- exos/2_syntax/part-2.js
+++ exos/2_syntax/part-2.js
@@ -10,51 +10,79 @@ console.log('--- 1 ---');
 /*
   créer une fonction 'x10' qui prend un nombre en entrée et renvoie ce nombre multiplié par 10.
 */
-
+function x10(input) {
+  return input * 10;
+}
+console.log(x10(3));
 console.log('--- 2 ---');
 /*
   créer un nouveau tableau à partir de 'nombres' contenant chaque nombre multiplié par 10, en utilisant 'x10'
   Utiliser une boucle for
 */
 const nombres = [23, 12, 57, 178, 139, 434, 222, 1000, 59, 887, 3141];
+const nombrex10 = [];
+//nombres.forEach(n => nombrex10.push(x10(n)));
+for (let i = 0; i < nombres.length; i++) {
+  nombrex10.push(x10(nombres[i]));
+}
+console.log(nombrex10);
 
 console.log('--- 2-bis ---');
-// la même chose avec une boucle for ... of
-
+const nombrex10_2 = [];
+for (const n of nombres) {
+  nombrex10_2.push(x10(n));
+}
+console.log(nombrex10_2)
 console.log('--- 2-ter ---');
 // la même chose avec .map
-
+const nombrex10_3 = [];
+nombres.map(n => nombrex10_3.push(x10(n)));
+console.log(nombrex10_3)
 console.log('--- 3 ---');
 /* [6] à partir de "nombres", créer un nouveau tableau avec seulement les entiers plus grands que 100
   en utilisant une boucle if
 */
-
+const h100 = [];
+nombres.map(n => {
+  if (n > 100) h100.push(n);
+});
+console.log(h100);
 console.log('--- 3-bis ---');
 // la même chose avec .filter
+const h100_2 = nombres.filter(n => n > 100);
+console.log(h100_2);
 
 console.log('--- 4 ---');
 /*
   à partir de "nombres", créer un nouveau tableau avec seulement les entiers pairs
   en utilisant .filter()
 */
-
+const even = nombres.filter(n => n % 2 === 0);
+console.log(even);
 console.log('--- 5 ---');
 // créer une fonction 'getLength' qui prend une string en entrée en renvoie sa longueur
-
+function getLength(str) {
+  return str.length;
+}
+console.log(getLength('Valentin'));
 console.log('--- 6 ---');
 /*
   logguer chacune des valeurs de "couleurs" et leur longueur dans la console
   en utilisant .forEach() et 'getLength'.
   ranger le résultat de couleurs.forEach(...) dans une variable '_longueurs'
 */
-
+const couleurs = ['Bleu', 'Rouge', 'Vert', 'Jaune', 'Violet', 'Noir', 'Magenta', 'Jaune citron', 'Bleu Saphir']
+const _longueurs = [];
+couleurs.forEach(c => _longueurs.push({ 'color': c, 'length': getLength(c) }));
+console.log(_longueurs)
 console.log('--- 7 ---');
 /*
   à partir de couleurs créer un nouveau tableau 'longueurs' contenant toutes les longueurs
   en utilisant .map() et 'getLength'.
   logguer '_longueurs' et 'longueurs', comprendre la différence entre .map et .forEach
 */
-
+const longueurs = couleurs.map(c => getLength(c));
+console.log(longueurs)
 console.log('');
 console.log("--- Si t'en as marre, c'est normal, c'est la fin ---");
 console.log('--- 1 ---');
@@ -62,19 +90,32 @@ console.log('--- 1 ---');
 ajouter 2 ou 3 nouvelles personnes au tableau "personnes", en utilisant .push()
 */
 const personnes = [{ nom: 'Romain', date: '1985-12-31' }];
+personnes.push({ nom: 'Valentin', date: '2000-11-11' });
+personnes.push({ nom: 'Léonardo', date: '1974-11-11' });
+personnes.push({ nom: 'George', date: '1885-11-11' });
+personnes.push({ nom: 'John', date: '2000-10-01'})
+console.log(personnes)
 
 console.log('--- 2 ---');
 /*
   à partir de "personnes", créer un nouveau tableau contenant seulement les noms en utilisant .map()
 */
+const names = personnes.map(p => p.nom);
+console.log(names)
 
 console.log('--- 3 ---');
 /*
   créer une fonction "calculerAge" qui en entrée prend un string représentant la date de naissance
   et renvoie l'âge en nombre, en utilisant Date()
 */
-
+function calculerAge(birth) {
+  let d = new Date(birth), n = new Date();
+  return  Math.floor((n - d) / 1000 / 3600 / 24 / 365.25);
+}
+console.log(calculerAge(personnes[4].date))
 console.log('--- 4 ---');
 /*
   à partir de "personnes" et "calculerAge", créer un nouveau tableau contenant seulement les âges
 */
+const age = personnes.map(p => calculerAge(p.date));
+console.log(age);

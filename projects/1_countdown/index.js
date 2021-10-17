40d42eea810dffa51c4a1b95b6d1b3c41121776c
Valentin Magry <valou32720@gmail.com>
2021-10-17 19:54:59 +0200

    non mais on est où la?????

--- projects/1_countdown/index.js
+++ projects/1_countdown/index.js
@@ -8,48 +8,55 @@
 const toReach = new Date('11-11-2021 12:00:00');
 const now = new Date();
 
-const HOURS = 24, MINUTES = 60, SECONDS = 60, MILLISECONDS = 1000;
+const MILLISECONDS = 1,
+    SECONDS = 1000 * MILLISECONDS,
+    MINUTES = 60 * SECONDS,
+    HOURS = 60 * MINUTES,
+    DAYS = 24 * HOURS
+
 const logElement = document.querySelector('#status')
 const delta = toReach - now;
 let timeLeft, timer;
-if(delta > 0){
+if (delta > 0) {
     timeLeft = SplitTime(delta);
+    console.log(timeLeft)
+
     timer = setInterval(Countdown, 10);
 }
-else{
+else {
     console.error('The date cannot be later than now');
     logElement.innerText = 'The date cannot be later than now';
     logElement.style.color = 'red';
 }
 
 
-function Countdown(){
-    if(timeLeft.milliseconds !== 0){
+function Countdown() {
+    if (timeLeft.milliseconds !== 0) {
         timeLeft.milliseconds--;
     }
-    else if(timeLeft.seconds !== 0){
+    else if (timeLeft.seconds !== 0) {
         timeLeft.seconds--;
         timeLeft.milliseconds = 99;
     }
-    else if(timeLeft.minutes !== 0){
+    else if (timeLeft.minutes !== 0) {
         timeLeft.minutes--;
-        timeLeft.seconds = SECONDS-1;
+        timeLeft.seconds = SECONDS - 1;
         timeLeft.milliseconds = 99;
     }
-    else if(timeLeft.hours !== 0){
+    else if (timeLeft.hours !== 0) {
         timeLeft.hours--;
-        timeLeft.minutes = MINUTES-1;
-        timeLeft.seconds = SECONDS-1;
-        timeLeft.milliseconds =  99;
+        timeLeft.minutes = MINUTES - 1;
+        timeLeft.seconds = SECONDS - 1;
+        timeLeft.milliseconds = 99;
     }
-    else if(timeLeft.days !== 0){
+    else if (timeLeft.days !== 0) {
         timeLeft.days--;
-        timeLeft.hours = HOURS-1;
-        timeLeft.minutes = MINUTES-1;
-        timeLeft.seconds = SECONDS-1;
-        timeLeft.milliseconds =  99;
+        timeLeft.hours = HOURS - 1;
+        timeLeft.minutes = MINUTES - 1;
+        timeLeft.seconds = SECONDS - 1;
+        timeLeft.milliseconds = 99;
     }
-    else{
+    else {
         logElement.style.fontSize = '10vw';
         logElement.innerText = 'Time reached !';
         logElement.style.color = 'green';
@@ -61,7 +68,7 @@ function Countdown(){
     PrintTime(timeLeft)
 }
 
-function PrintTime(t){
+function PrintTime(t) {
     document.querySelector('#days').innerText = t.days;
     document.querySelector('#hours').innerText = t.hours;
     document.querySelector('#minutes').innerText = t.minutes;
@@ -70,20 +77,11 @@ function PrintTime(t){
 }
 
 function SplitTime(delta) {
-    let d = Math.floor(delta / HOURS / MINUTES / SECONDS / MILLISECONDS);
-    delta -= d * HOURS * MINUTES * SECONDS * MILLISECONDS;
-
-    let h = Math.floor(delta / MINUTES / SECONDS / MILLISECONDS);
-    delta -= h * MINUTES * SECONDS * MILLISECONDS;
-
-    let m = Math.floor(delta / SECONDS / MILLISECONDS);
-    delta -= m * SECONDS * MILLISECONDS;
-
-    let s = Math.floor(delta / MILLISECONDS);
-    delta -= s * MILLISECONDS;
-
-    let ms = Math.round(delta / 10);
-
+    const d = Math.floor(delta / DAYS);
+    const h = Math.floor((delta % DAYS) / HOURS);
+    const m = Math.floor((delta % HOURS) / MINUTES);
+    const s = Math.floor((delta % MINUTES) / SECONDS);
+    const ms = Math.round(delta % SECONDS / 10);
     return { days: d, hours: h, minutes: m, seconds: s, milliseconds: ms }
 }
 
